fe27ad7bf36ec4e579da1708731734acfda89a85
EC2 Default User <ec2-user@ip-172-31-45-181.us-east-2.compute.internal>
2020-05-30 02:32:33 +0000

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -6,6 +6,11 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     @base_title = "Ruby on Rails Tutorial Sample App"
   end
   
+  test "should get root" do
+    get root_url
+    assert_response :success
+  end
+  
   test "should get home" do
     get static_pages_home_url
     assert_response :success
@@ -27,7 +32,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get contact" do
     get static_pages_contact_url
     assert_response :success
-    assert_select "title", "About | #{@base_title}"
+    assert_select "title", "Contact | #{@base_title}"
   end
 
 end
